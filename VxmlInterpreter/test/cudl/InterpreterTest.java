3da65f6ed2c73d164c8a3a566379d218e96534dc
Pascal Grange <pascal@grange.nom.fr>
2011-10-19 09:40:23 +0200

    CHG simpler url scheme for tests

--- VxmlInterpreter/test/cudl/InterpreterTest.java
+++ VxmlInterpreter/test/cudl/InterpreterTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -22,7 +21,7 @@ public class InterpreterTest {
 
 	@Before
 	public void setUp() throws IOException {
-		url = "file://" + new File(".").getCanonicalPath() + "/test/docVxml/";
+		url = "file:test/docVxml/";
 	}
 
 	@Test
